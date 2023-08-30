83152592b9323b7e414dc1b4e576a2860d4e49b2
needlessbruiser <115527661+needlessbruiser@users.noreply.github.com>
2023-08-30 12:17:45 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Explore Gun Laws with TheGunLaws.com</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            line-height: 1.6;
+            margin: 0;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            min-height: 100vh;
+            background-color: #f2f2f2;
+        }
+
+        .container {
+            max-width: 1200px;
+            text-align: center;
+            padding: 20px;
+        }
+
+        h1 {
+            color: #0066cc;
+            margin-bottom: 20px;
+        }
+
+        p {
+            font-size: 18px;
+            color: #333;
+            text-align: center;
+            margin: 10px 0;
+            line-height: 1.8;
+        }
+
+        a {
+            color: #0066cc;
+            text-decoration: none;
+        }
+
+        a:hover {
+            text-decoration: underline;
+        }
+
+        button {
+            background-color: #0066cc;
+            color: #fff;
+            border: none;
+            padding: 10px 20px;
+            font-size: 16px;
+            cursor: pointer;
+        }
+
+        button:hover {
+            background-color: #0052a3;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="container">
+        <h1>Explore Gun Laws with TheGunLaws.com</h1>
+        <p>TheGunLaws.com provides valuable insights into gun laws and regulations. Whether you're curious about ownership, permits, or rights, we have you covered. Get informed about concealed carry laws and your rights as a gun owner.</p>
+        <p>For more details, visit <a href="https://thegunlaws.com/">TheGunLaws.com</a> or find information about <a href="https://thegunlaws.com/">Concealed carry</a>.</p>
+
+        <div>
+        </div>
+        <a href="https://www.facebook.com/ahsanghaffar23/" target="_blank"><button>Connect on Facebook</button></a>
+
+    </div>
+</body>
+
+</html>
